46cc6272e034d77e548ce45cec8e2fe4619d75df
maheshahir6118 <maheshnandaniyam@gmail.com>
2026-08-04 12:05:28 +0530

    Update urls.py

--- doctor_finder/doctor_finder/urls.py
+++ doctor_finder/doctor_finder/urls.py
@@ -16,12 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.http import JsonResponse 
-def home(request):
-    return JsonResponse({"status": "API is running"})
+
 
 urlpatterns = [
-    path('', home),
     path('admin/', admin.site.urls),
     path('api/',include('doctors.urls')),
 ]
